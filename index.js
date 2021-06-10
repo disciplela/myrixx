2e7786cb2ef9894c90c0c51488bc23896e0fcb42
Sean M <seansms@protonmail.com>
2021-06-10 12:05:57 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Overlay Timer toggle functions
+
+function on() {
+  document.getElementById("test").style.display = "block";
+}
+
+function off() {
+  document.getElementById("test").style.display = "none";
+}
